43466ae16b2dcf2d486d24329487ddf51c0ca331
Swapnil Renge <swapnil.renge@globant.com>
2024-10-06 18:30:03 +0530

    feat: update server to use cors and static server as  middlewares

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,18 @@
+import cors from "cors";
 import express from "express";
-
 import errorHandler from "./middlewares/errorHandler.js";
 import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 
+// middleware goes here
+
 app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.static("public"));
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+
+// API routes goes here
 app.use("/api/users", userRoutes);
 app.use(errorHandler);
 
